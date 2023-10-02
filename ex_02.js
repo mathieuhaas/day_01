5e546779d655d916d174d0676e7e1cf4ac0d1916
mathieuhaas <146095811+mathieuhaas@users.noreply.github.com>
2023-10-02 17:42:18 +0200

    ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,10 @@
+let bool = true
+let string = "forty two" 
+let integer = 42 
+let float = 42.42 
+let this_is_null = null;
+displayThisText(bool);
+displayThisText(string);
+displayThisText(integer);
+displayThisText(float);
+displayThisText(this_is_null);
